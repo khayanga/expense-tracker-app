53e230322f89d88b650bd8fb305ff9a1762155c5
khayanga <khayangavirgil@gmail.com>
2025-09-08 22:33:34 +0300

    Transactions endpoints

--- backend/src/controllers/transactionController.js
+++ backend/src/controllers/transactionController.js
@@ -50,11 +50,125 @@ export async function getTransactionById(req, res) {
             return res.status(404).json({error:"Transaction not found"})
         }
         res.status(200).json(transcation)
-        console.log("Transaction fetched successfully");
+        
     }
     catch(error){
         console.error("Error fetching transaction", error);
         res.status(500).json({error:"Internal server error"})
     }
     
+}
+
+export async function getTransactionsByUserId(req, res) {
+  const { user_id } = req.params;
+  try {
+    const transactions = await db.transactions.findMany({
+      where: { user_id }
+    });
+
+    if (!transactions || transactions.length === 0) {
+      return res.status(404).json({ error: "No transactions found for this user" });
+    }
+
+    res.status(200).json(transactions);
+  } catch (error) {
+    console.error("Error fetching transactions", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+}
+
+
+export async function updateTransaction(req, res) {
+    const {id} = req.params;
+    const {amount, category, title} = req.body;
+    try {
+
+      const existingTransaction = await db.transactions.findUnique(
+        {where : {id:Number(id)}}
+      )
+      if(!existingTransaction){
+        return res.status(404).json({error:"Transaction not found"})
+      }
+
+      const updateTransaction = await db.transactions.update({
+        where :{id: Number(id)},
+        data:{
+          amount,
+          category,
+          title
+        }
+      })
+      res.status(200).json(updateTransaction)
+      
+      
+    } catch (error) {
+      console.error("Error updating transaction", error);
+      res.status(500).json({error:"Internal server error"})
+      
+    }
+      
+}
+
+export async function deleteTransaction(req, res) {
+  const { id } = req.params;
+
+  try {
+    const existingTransaction = await db.transactions.findUnique({
+      where: { id: Number(id) }
+    });
+
+    if (!existingTransaction) {
+      return res.status(404).json({ error: "Transaction not found" });
+    }
+
+    const deletedTransaction = await db.transactions.delete({
+      where: { id: Number(id) }
+    });
+
+     res.status(200).json({
+      message: "Transaction deleted successfully",
+      deletedTransaction
+    });
+  } catch (error) {
+    console.error("Error deleting transaction", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+}
+
+export async function getSummary(req, res) {
+  try {
+    const { userId } = req.params;
+
+    // total balance
+    const balanceAgg = await db.transactions.aggregate({
+      _sum: { amount: true },
+      where: { user_id: userId },
+    });
+
+    // income (amount > 0)
+    const incomeAgg = await db.transactions.aggregate({
+      _sum: { amount: true },
+      where: { user_id: userId, amount: { gt: 0 } },
+    });
+
+    // expenses (amount < 0)
+    const expensesAgg = await db.transactions.aggregate({
+      _sum: { amount: true },
+      where: { user_id: userId, amount: { lt: 0 } },
+    });
+
+    const balance = balanceAgg._sum.amount || 0;
+    const income = incomeAgg._sum.amount || 0;
+    const expenses = Math.abs(expensesAgg._sum.amount || 0);
+
+    res.status(200).json({
+      message: "Transaction summary fetched successfully",
+      balance,
+      income,
+      expenses,
+    });
+  } catch (error) {
+    console.error("Error getting the summary:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 }
--- backend/src/routes/transactionRoute.js
+++ backend/src/routes/transactionRoute.js
@@ -1,11 +1,15 @@
 
 import express from 'express';
-import { createTransaction, getTransactionById, getTransactions } from '../controllers/transactionController.js';
+import { createTransaction, deleteTransaction, getSummary, getTransactionById, getTransactions,  getTransactionsByUserId,  updateTransaction } from '../controllers/transactionController.js';
 
 const router = express.Router();
 
 router.post("/", createTransaction)
 router.get("/", getTransactions)
+router.get("/:user_id", getTransactionsByUserId)
 router.get("/:id", getTransactionById)
+router.patch("/:id", updateTransaction)
+router.delete("/:id", deleteTransaction)
+router.get("/summary/:user_id", getSummary)
 
 export default router;
